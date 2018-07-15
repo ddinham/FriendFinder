d8e11a295000452a4cb2eff28215c1bad5379e41
Dawn Bondy <dinham.dawn@gmail.com>
2018-07-14 23:59:07 -0400

    new

---
+++ app/data/friends.js
@@ -0,0 +1,266 @@
+// ===============================================================================
+// DATA
+// Below data will hold all the answers to the survey.
+// ===============================================================================
+
+var friendArray = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "scores": [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+        ]
+        },
+        {
+        "name": "Jacob Deming",
+        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "scores": [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+        ]
+        },
+        {
+        "name": "Jeremiah Scanlon",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+        ]
+        },
+        {
+        "name": "Louis T. Delia",
+        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "scores": [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+        ]
+        },
+        {
+        "name": "Lou Ritter",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        "scores": [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+        ]
+        },
+        {
+        "name": "Jordan Biason",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        "scores": [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+        ]
+        },
+        {
+        "name": "Alexander Stewart",
+        "photo": "https://i.imgur.com/OCzMux0.jpg",
+        "scores": [
+        "4",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "4",
+        "1",
+        "5",
+        "5"
+        ]
+        },
+        {
+        "name": "Alexander Stewart",
+        "photo": "https://i.imgur.com/OCzMux0.jpg",
+        "scores": [
+        "4",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "4",
+        "1",
+        "5",
+        "5"
+        ]
+        },
+        {
+        "name": "Alexander Stewart",
+        "photo": "https://i.imgur.com/OCzMux0.jpg",
+        "scores": [
+        "4",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "4",
+        "1",
+        "5",
+        "5"
+        ]
+        },
+        {
+        "name": "jbalasingam",
+        "photo": "jooj",
+        "scores": [
+        "2",
+        "3",
+        "2",
+        "3",
+        "2",
+        "3",
+        "3",
+        "2",
+        "3",
+        "2"
+        ]
+        },
+        {
+        "name": "test",
+        "photo": "test",
+        "scores": [
+        "2",
+        "2",
+        "2",
+        "1",
+        "2",
+        "3",
+        "4",
+        "2",
+        "1",
+        "2"
+        ]
+        },
+        {
+        "name": "ed",
+        "photo": "a",
+        "scores": [
+        "5",
+        "2",
+        "5",
+        "3",
+        "5",
+        "5",
+        "5",
+        "3",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "Ryan Bryce",
+        "photo": "https://http.cat/405",
+        "scores": [
+        "2",
+        "3",
+        "4",
+        "5",
+        "4",
+        "3",
+        "3",
+        "2",
+        "3",
+        "2"
+        ]
+        },
+        {
+        "name": "Ryan Bryce",
+        "photo": "https://http.cat/405",
+        "scores": [
+        "2",
+        "3",
+        "4",
+        "5",
+        "4",
+        "3",
+        "3",
+        "2",
+        "3",
+        "2"
+        ]
+        },
+        {
+        "name": "Ramon",
+        "photo": "http://google.com",
+        "scores": [
+        "2",
+        "3",
+        "4",
+        "4",
+        "2",
+        "4",
+        "5",
+        "1",
+        "2",
+        "3"
+        ]
+        },
+        {
+        "name": "Lil Mac",
+        "photo": "https://vignette.wikia.nocookie.net/punchout/images/0/0f/Little_Mac_artwork.png/revision/latest?cb=20140412011702&path-prefix=es",
+        "scores": [
+        "3",
+        "4",
+        "3",
+        "5",
+        "3",
+        "1",
+        "3",
+        "4",
+        "3",
+        "2"
+        ]
+        }
+];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendArray;
